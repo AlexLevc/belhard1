64906ca7366b103a24ef0d8ae1a18f83f3cbf917
AlexLevc <matemet772@gmail.com>
2022-09-07 09:24:05 +0300

    3-4 complite

--- 3-2.py
+++ 3-2.py
@@ -1,7 +1,7 @@
 #пользователь вводит 3 числа, найти среднее арифметическое с точностью 3
-num1 = int(input())
-num2 = int(input())
-num3 = int(input())
+num1 = int(input('1-е число '))
+num2 = int(input('2-е число '))
+num3 = int(input('3-е число '))
 sum = (num1 + num2 + num3)/3
 sum = round(sum,3)
 print(sum)
