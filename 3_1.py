843317933cd8057f75d7172f8a77905207bb3342
AlexLevc <matemet772@gmail.com>
2022-09-07 09:22:32 +0300

    3-4 complite

--- 3_1.py
+++ 3_1.py
@@ -1,8 +1,7 @@
+#пользователь вводит предложение, заменить все пробелы на "-" 2-мя способами
 text1 = input('text1 = ')
 word = text.split()
 text1 = '-'.join(word)
 print(text1)
-
-
 text2 = input().replace(' ', '-')
 print(text2)
