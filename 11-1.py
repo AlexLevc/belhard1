b10b1da7302f9596eeeb27cb13144c9508e7a0e9
AlexLevc <matemet772@gmail.com>
2022-10-11 15:55:34 +0300

    11-1 complite update

--- 11-1.py
+++ 11-1.py
@@ -5,34 +5,34 @@ cur = conn.cursor()
 
 cur.execute('''
 CREATE TABLE IF NOT EXISTS statuses(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(10) UNIQUE
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(10) NOT NULL UNIQUE
 );
 ''')
 conn.commit()
 
 cur.execute('''
 CREATE TABLE IF NOT EXISTS users(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(24),
-    email VARCHAR(24) UNIQUE
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(24) NOT NULL,
+    email VARCHAR(24) NOT NULL UNIQUE
 );
 ''')
 conn.commit()
 
 cur.execute('''
 CREATE TABLE IF NOT EXISTS categories(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(24) UNIQUE
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(24) NOT NULL UNIQUE
  );
  ''')
 conn.commit()
 
 cur.execute('''
 CREATE TABLE IF NOT EXISTS orders(
-    id INTEGER PRIMARY KEY,
-    user_id INTEGER,
-    status_id INTEGER,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    status_id INTEGER NOT NULL,
     FOREIGN KEY (status_id) REFERENCES statuses(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
@@ -40,19 +40,19 @@ CREATE TABLE IF NOT EXISTS orders(
 conn.commit()
 
 cur.execute('''CREATE TABLE IF NOT EXISTS products(
-    id INTEGER PRIMARY KEY,
-    title VARCHAR(36),
-    description VARCHAR(140),
-    category_id INTEGER,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(36) NOT NULL ,
+    description VARCHAR(140) NOT NULL ,
+    category_id INTEGER NOT NULL ,
     FOREIGN KEY (category_id) REFERENCES categories(id)
 ); ''')
 conn.commit()
 
 cur.execute('''
 CREATE TABLE IF NOT EXISTS order_items(
-    id INTEGER PRIMARY KEY,
-    order_id INTEGER,
-    product_id INTEGER,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    order_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
     FOREIGN KEY (order_id) REFERENCES orders(id),
     FOREIGN KEY (product_id) REFERENCES products(id)
     );
